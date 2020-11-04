304cb219ec25582c3d1fddf23427f8b19e03ef06
Fedor Indutny <fedor@indutny.com>
2020-11-03 19:35:14 -0800

    save

--- mix.exs
+++ mix.exs
@@ -17,6 +17,7 @@ defmodule Funnel.MixProject do
   # Run "mix help deps" for examples and options.
   defp deps do
     [
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 end
