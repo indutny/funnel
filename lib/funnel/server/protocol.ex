d0c209e5d3ecd718975e34d14f77e8238d8404ff
Fedor Indutny <fedor@indutny.com>
2021-04-22 13:55:29 -0700

    server: continue port to ranch

--- lib/funnel/server/protocol.ex
+++ lib/funnel/server/protocol.ex
@@ -2,12 +2,14 @@ defmodule Funnel.Server.Protocol do
   use Task
   use TypedStruct
 
-  @type socket() :: :ranch_transport.socket()
   @type transport() :: module()
 
   require Logger
 
   typedstruct module: Config do
+    field :transport, module()
+    field :remote, :ranch_transport.socket()
+
     field :local_domain, String.t(), default: "funnel.localhost"
     field :max_mail_size, non_neg_integer(), default: 30 * 1024 * 1024
     # 5 minutes
@@ -42,65 +44,76 @@ defmodule Funnel.Server.Protocol do
         mail_scheduler: {Funnel.MailScheduler, Funnel.MailScheduler}
       })
 
-    send_response(remote, transport, SMTPServer.handshake(conn))
+    config = %Config{config| transport: transport, remote: remote}
+    send_response(config, SMTPServer.handshake(conn))
 
-    serve(remote, transport, conn, config)
+    serve(config, conn, <<>>)
   end
 
-  @spec serve(socket(), transport(), SMTPServer.t(), Config.t()) :: nil
-  def serve(remote, transport, conn, config) do
-    case transport.recv(remote, 0, config.read_timeout) do
+  @spec serve(Config.t(), SMTPServer.t(), binary()) :: nil
+  def serve(config, conn, buffer) do
+    case config.transport.recv(config.remote, 0, config.read_timeout) do
       {:ok, packet} ->
-        IO.puts(packet)
-        line = "ohai"
-        response = SMTPServer.respond_to(conn, line)
-
-        send_response(remote, transport, response)
-        serve(remote, transport, conn, config)
+        buffer = buffer <> packet
+        buffer = read_line(config, conn, buffer)
+        serve(config, conn, buffer)
 
       {:error, :closed} ->
         exit(:shutdown)
     end
   end
 
-  @spec send_response(socket(), transport(), SMTPServer.response()) :: nil
-  defp send_response(_, _, :no_response) do
+  @spec read_line(Config.t(), SMTPServer.t(), binary()) :: binary()
+  def read_line(config, conn, buffer) do
+    case String.split(buffer, ~r/(?<=\r\n|\n)/, parts: 2) do
+      [line, buffer] ->
+        response = SMTPServer.respond_to(conn, line)
+        send_response(config, response)
+
+        read_line(config, conn, buffer)
+
+      [buffer] ->
+        buffer
+    end
+  end
+
+  @spec send_response(Config.t(), SMTPServer.response()) :: nil
+  defp send_response(_, :no_response) do
     # no-op
   end
 
-  defp send_response(remote, transport, {mode, code, line}) when is_bitstring(line) do
-    send_response(remote, transport, :last, {mode, code, line})
+  defp send_response(config, {mode, code, line}) when is_bitstring(line) do
+    send_response(config, :last, {mode, code, line})
   end
 
-  defp send_response(remote, transport, {mode, code, [line]}) do
-    send_response(remote, transport, :last, {mode, code, line})
+  defp send_response(config, {mode, code, [line]}) do
+    send_response(config, :last, {mode, code, line})
   end
 
-  defp send_response(remote, transport, {mode, code, [line | rest]}) do
-    send_response(remote, transport, :not_last, {mode, code, line})
-    send_response(remote, transport, {mode, code, rest})
+  defp send_response(config, {mode, code, [line | rest]}) do
+    send_response(config, :not_last, {mode, code, line})
+    send_response(config, {mode, code, rest})
   end
 
   @spec send_response(
-          socket(),
-          transport(),
+    Config.t(),
           :not_last | :last,
           SMTPServer.response()
         ) :: nil
-  defp send_response(remote, transport, order, {mode, code, line}) do
+  defp send_response(config, order, {mode, code, line}) do
     response =
       case order do
         :not_last -> "#{code}-#{line}\r\n"
         :last -> "#{code} #{line}\r\n"
       end
 
-    case transport.send(remote, response) do
+    case config.transport.send(config.remote, response) do
       :ok -> :ok
       {:error, :closed} -> exit(:shutdown)
     end
 
     if order == :last and mode == :shutdown do
-      :ok = transport.shutdown(remote, :write)
+      :ok = config.transport.shutdown(config.remote, :write)
     end
   end
 end
